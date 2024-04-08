8dff0caceb9c0a6e2a22360d59329b97862b0c98
PberruezoArenas <149729949+PberruezoArenas@users.noreply.github.com>
2024-04-08 00:01:54 +0000

    css project

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,32 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import "../../styles/generalstyles.css";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
+		<h1>Dar de baja</h1>
+		<h2>1º) INFORMACIÓN SOBRE PROPIEDAD INTELECTUAL E INDUSTRIAL</h2>
+		<h3>CONDICIONES Y TÉRMINOS DE REGISTRO Y ACCESO AL SITIO WEB.</h3>	
+		<h4>Dar de baja</h4>	
+		<h5>Dar de baja</h5>	
+		<h6>Dar de baja</h6>	
+		<p>La recepción por parte de FUNTSAK de la comunicación prevista en esta cláusula no supondrá, según lo dispuesto en la LSSI, el conocimiento efectivo de las actividades y/o contenidos indicados por el enlace en párrafos comunicante.</p>
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
+		<button className="button1"> Crear Usuario</button>
 	</div>
 );
+
+
+// 1º) INFORMACIÓN SOBRE PROPIEDAD INTELECTUAL E INDUSTRIAL
+ 
+// 2º) CONDICIONES Y TÉRMINOS DE REGISTRO Y ACCESO AL SITIO WEB. 
+ 
+// 3º) OBJETO 
+ 
+// 4º) POLÍTICA DE PROTECCIÓN DE DATOS PERSONALES. 
+
+// 5º) VINCULACION A OTROS SITIOS WEB. 
+
+// La recepción por parte de FUNTSAK de la comunicación prevista en esta cláusula no supondrá, según lo dispuesto en la LSSI, el conocimiento efectivo de las actividades y/o contenidos indicados por el enlace en párrafos comunicante.
