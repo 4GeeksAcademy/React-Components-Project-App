d7c53b375c67d28d8edf34bd898641d87c3104dd
PberruezoArenas <149729949+PberruezoArenas@users.noreply.github.com>
2024-04-08 10:38:00 +0000

    css project general styles 2

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,15 +6,31 @@ export const Home = () => (
 	<div className="text-center mt-5">
 		<h1>Dar de baja</h1>
 		<h2>1º) INFORMACIÓN SOBRE PROPIEDAD INTELECTUAL E INDUSTRIAL</h2>
-		<h3>CONDICIONES Y TÉRMINOS DE REGISTRO Y ACCESO AL SITIO WEB.</h3>	
-		<h4>Dar de baja</h4>	
-		<h5>Dar de baja</h5>	
-		<h6>Dar de baja</h6>	
+		<h3>2º) CONDICIONES Y TÉRMINOS DE REGISTRO Y ACCESO AL SITIO WEB.</h3>	
+		<h4>3º) OBJETO </h4>	
+		<h5>4º) POLÍTICA DE PROTECCIÓN DE DATOS PERSONALES.</h5>	
+		<h6>5º) VINCULACION A OTROS SITIOS WEB. </h6>	
 		<p>La recepción por parte de FUNTSAK de la comunicación prevista en esta cláusula no supondrá, según lo dispuesto en la LSSI, el conocimiento efectivo de las actividades y/o contenidos indicados por el enlace en párrafos comunicante.</p>
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+		</ul>
+
+		<ol>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+		</ol>
+
 		<button className="button1"> Crear Usuario</button>
+		<br />
+		<button className="button2"> ^ </button>
+		<br />
+		<button className="button3"> Contactar con Terapeuta</button>
 	</div>
 );
 
