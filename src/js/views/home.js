99ed2c6ae2bdb6d80640fb3de21ce1d80af55d5c
PberruezoArenas <149729949+PberruezoArenas@users.noreply.github.com>
2024-04-09 22:41:36 +0000

    add photos, button to up

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,33 +1,88 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
+import landing1 from "../../img/landing1.png";
+import sad from "../../img/sad.png";
+import skate from "../../img/skate.png";
+import emotions from "../../img/emotions.png";
 import "../../styles/generalstyles.css";
 import "../../styles/landingpage.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
-
-
+import { useEffect } from "react";
 
 
 
 export const Home = () => {
 
+	useEffect(() => {
+        const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+        // Agregar evento de clic al botón
+        scrollToTopBtn.addEventListener("click", () => {
+            scrollToTop(0, 500); // Llama a la función scrollToTop con destino 0 y duración 500ms
+        });
+
+        // Función para el desplazamiento suave
+        function scrollToTop(to, duration) {
+            const element = document.scrollingElement || document.documentElement;
+            const start = element.scrollTop;
+            const change = to - start;
+            let currentTime = 0;
+            const increment = 20; // Incremento de tiempo en milisegundos para cada paso de animación
+
+            function animateScroll() {
+                currentTime += increment;
+                const val = easeInOutQuad(currentTime, start, change, duration);
+                element.scrollTop = val;
+                if (currentTime < duration) {
+                    requestAnimationFrame(animateScroll);
+                } else {
+                    element.scrollTop = to;
+                }
+            }
+
+            function easeInOutQuad(t, b, c, d) {
+                t /= d / 2;
+                if (t < 1) return c / 2 * t * t + b;
+                t--;
+                return -c / 2 * (t * (t - 2) - 1) + b;
+            }
+
+            animateScroll();
+        }
+
+        // Limpiar el event listener cuando el componente se desmonta
+        return () => {
+            scrollToTopBtn.removeEventListener("click", () => {
+                scrollToTop(0, 500);
+            });
+        };
+    }, []); 
+
+	
+
 	return (
 
-		<Container fluid className="container-landingpage container">
+		<Container fluid className="container-landingpage">
+			<button id="scrollToTopBtn" >↑</button>
 
 			<Container>
 				<Row className="align-items-center">
-					<Col xs={1} md={3} className="text-center">
-						<img src={rigoImage} alt="Image 1" className="image-top-left-container1" />
-						<img src={rigoImage} alt="Image 2" className="image-bottom-left-container1" />
+					<Col xs={1} md={3} className="text-center align-items-center">
+					<div className="d-flex flex-column align-items-center">
+						<img src={landing1} alt="Image 1" className="image-top-left-container1" />
+						<img src={sad} alt="Image 2" className="image-bottom-left-container1" />
+					</div>
 					</Col>
 					<Col xs={10} md={6} className="text-center">
 						<h1 className="heading1">Bienvenidxs a</h1>
 						<h2 className="heading2">la Comunidad <br />de Apoyo <br />Emocional</h2>
 					</Col>
-					<Col xs={1} md={3} className="text-center">
-						<img src={rigoImage} alt="Image 3" className="image-top-right-container1" />
-						<img src={rigoImage} alt="Image 4" className="image-bottom-right-container1" />
+					<Col xs={1} md={3} >
+					<div className="d-flex flex-column align-items-center">
+                    	<img src={skate} alt="Image 3" className="image-top-right-container1" />
+                    	<img src={emotions} alt="Image 4" className="image-bottom-right-container1"/>
+                	</div>
 					</Col>
 				</Row>
 			</Container>
