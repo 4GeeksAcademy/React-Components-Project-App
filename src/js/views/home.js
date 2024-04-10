b39ea7c174bb5da6794c183000e1290546226063
PberruezoArenas <149729949+PberruezoArenas@users.noreply.github.com>
2024-04-10 22:20:28 +0000

    add photos landing

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,10 +4,16 @@ import landing1 from "../../img/landing1.png";
 import sad from "../../img/sad.png";
 import skate from "../../img/skate.png";
 import emotions from "../../img/emotions.png";
+import photos from "../../img/photos.png";
+import road from "../../img/road.png";
+import angry from "../../img/angry.png";
+import serious from "../../img/serious.png";
+import happy from "../../img/happy.png";
+import pills from "../../img/pills.png";
 import "../../styles/generalstyles.css";
 import "../../styles/landingpage.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, } from 'react-bootstrap';
 import { useEffect } from "react";
 
 
@@ -70,8 +76,8 @@ export const Home = () => {
 				<Row className="align-items-center">
 					<Col xs={1} md={3} className="text-center align-items-center">
 					<div className="d-flex flex-column align-items-center">
-						<img src={landing1} alt="Image 1" className="image-top-left-container1" />
-						<img src={sad} alt="Image 2" className="image-bottom-left-container1" />
+						<img src={landing1} alt="Image 1" className="image-top-left-container1" fluid/>
+						<img src={sad} alt="Image 2" className="image-bottom-left-container1" fluid/>
 					</div>
 					</Col>
 					<Col xs={10} md={6} className="text-center">
@@ -80,8 +86,8 @@ export const Home = () => {
 					</Col>
 					<Col xs={1} md={3} >
 					<div className="d-flex flex-column align-items-center">
-                    	<img src={skate} alt="Image 3" className="image-top-right-container1" />
-                    	<img src={emotions} alt="Image 4" className="image-bottom-right-container1"/>
+                    	<img src={skate} alt="Image 3" className="image-top-right-container1" fluid/>
+                    	<img src={emotions} alt="Image 4" className="image-bottom-right-container1" fluid/>
                 	</div>
 					</Col>
 				</Row>
@@ -109,8 +115,10 @@ export const Home = () => {
 							es un refugio seguro donde puedes expresar tus emociones, encontrar comprensión y acceder a recursos que enriquecen tu bienestar mental.
 						</p> <br /><br />
 					</Col>
-					<Col xs={12} md={4} className="d-flex justify-content-center align-items-center" s >
-						<img src={rigoImage} alt="Image 5" className="image-third-container-landingpage"/>	
+					<Col xs={12} md={4} className="d-flex justify-content-center align-items-center" >
+						<div className="d-flex flex-column align-items-center">
+							<img src={photos} alt="Image 5" className="image-third-container-landingpage" fluid/>
+						</div>	
 					</Col>
 				</Row>
 			</Container>
@@ -119,20 +127,31 @@ export const Home = () => {
 			<Container fluid className="fourth-container-landingpage">
 				<Row >
 					
-					<Col xs={12} md={3} className="d-flex justify-content-center align-items-center" s >
-						<img src={rigoImage} alt="Image 5" className="image-third-container-landingpage"/>	
+					<Col xs={12} md={3} className="d-flex justify-content-center align-items-center" >
+						<div className="d-flex flex-column align-items-center">
+							<img src={road} alt="Image 5" className="image-fourth-container-landingpage" fluid/>	
+						</div>
 					</Col>
 
-					<Col xs={12} md={7} className="text-left">
-						<h1 className="heading-fourth-container">Tu recorrido emocional es nuestro compromiso:</h1>
-						<p className="base-paragrahp">MyMood te permite llevar un diario emocional para visualizar tu evolución. 
-						¿Buscas apoyo? Conéctate con expertos en salud mental de forma discreta. 
-						Cada paso hacia la comprensión de tus emociones es un paso hacia tu felicidad.
-						</p> <br /><br />
+					<Col xs={12} md={8} className="text-left">
+					
+						<div className="fourth-container">
+							<h1 className="heading-fourth-container">Tu recorrido emocional es nuestro compromiso:</h1>
+							<p className="base-paragrahp">MyMood te permite llevar un diario emocional para visualizar tu evolución. 
+							¿Buscas apoyo? Conéctate con expertos en salud mental de forma discreta. 
+							Cada paso hacia la comprensión de tus emociones es un paso hacia tu felicidad.
+							Cada paso hacia la comprensión de tus emociones es un paso hacia tu felicidad.
+							Cada paso hacia la comprensión de tus emociones es un paso hacia tu felicidad.
+							</p> <br /><br />
+						</div>
 					</Col>
 
-					<Col xs={12} md={2} className="d-flex justify-content-center align-items-center" s >
-						<img src={rigoImage} alt="Image 5" className="image-third-container-landingpage"/>	
+					<Col xs={12} md={1} className="d-flex justify-content-center align-items-center" >
+						<div className="d-flex flex-column align-items-center">
+							<img src={happy} alt="Image 5" className="image-happy-fiveth-container-landingpage" fluid/>	
+							<img src={serious} alt="Image 5" className="image-serious-fiveth-container-landingpage" fluid/>	
+							<img src={angry} alt="Image 5" className="image-angry-fiveth-container-landingpage" fluid/>	
+						</div>
 					</Col>
 
 				</Row>
@@ -141,15 +160,17 @@ export const Home = () => {
 
 			<Container fluid className="fiveth-container-landingpage">
 				<Row >
-					<Col xs={12} md={8} className="text-left">
-						<h1 className="heading-thirdcontainer">Unidos en la diversidad emocional </h1>
+					<Col xs={12} md={7} className="text-left">
+						<h1 className="heading-fivethcontainer">Unidos en la diversidad emocional </h1>
 						<p className="base-paragrahp">MyMood te permite llevar un diario emocional para visualizar tu evolución. 
 						¿Buscas apoyo? Conéctate con expertos en salud mental de forma discreta. 
 						Cada paso hacia la comprensión de tus emociones es un paso hacia tu felicidad.
 						</p> <br /><br />
 					</Col>
-					<Col xs={12} md={4} className="d-flex justify-content-center align-items-center" s >
-						<img src={rigoImage} alt="Image 5" className="image-third-container-landingpage"/>	
+					<Col xs={12} md={5} className="d-flex justify-content-center align-items-center" >
+						<div className="d-flex flex-column align-items-center">
+							<img src={pills} alt="Image 5" className="image-sixth-container-landingpage" fluid/>
+						</div>			
 					</Col>
 				</Row>
 			</Container>
@@ -163,7 +184,7 @@ export const Home = () => {
 							Diseñada pensando en la juventud, nuestra plataforma es más que una simple red social;
 							es un refugio seguro donde puedes expresar tus emociones, encontrar comprensión y acceder a recursos que enriquecen tu bienestar mental.
 						</p> <br /><br />
-						<button className="button-sixth-container-landingpage"> Únete a Mymood ¡hoy!</button>
+						<button className="button1 button-second-container-landingpage"> Únete a Mymood ¡hoy!</button>
 					</Col>
 					
 				</Row>
